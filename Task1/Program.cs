b463d37e38ce9a4aca9987a81621676d430deb94
Bildushkina Tuyana <Budaeva.tuyana.garmaevna@gmail.com>
2022-08-21 15:17:12 +0800

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int number = a;
+
+for (int i = 1; i < b; i++)
+{
+    number = number * a;
+}
+
+Console.WriteLine($"A в степени B равно {number}.");
